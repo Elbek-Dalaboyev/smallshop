40846dfadf625356e525f548b51690d69a834a76
Elbek Dalaboyev <dalaboyeve@gmail.com>
2021-06-16 18:40:16 +0500

    first commit

---
+++ src/components/Product.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Product(props) {
+    const { product } = props
+    return (
+        <div>
+            <img className={'small'} src={product.image} alt={product.name}/>
+            <h3>{product.name}</h3>
+            <div>${product.price}</div>
+            <div>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+    )
+}
+
+export default Product
